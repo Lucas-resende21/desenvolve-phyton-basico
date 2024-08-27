ff6044fa5e0f3d29f92a166551671e1e6999dbd9
Lucas Resende <112007865+Lucas-resende21@users.noreply.github.com>
2024-08-27 11:22:04 -0300

    ex2

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "o resultado é"
+x = 3.5
+y = 10
+soma1 = x + y
+print(texto)
+print(type(texto))
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(soma1)
+print(type(soma1))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,5 @@
+rendimento = 500*1.01
+rendimento = rendimento*1.01
+rendimento = rendimento*1.01
+print("Após 3 meses meu novo saldo é")
+print(rendimento)
